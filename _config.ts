28401fe8498257cc48cb4e899ed4048cdfad5f16
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-06-30 11:52:59 +0100

    Adding deno site

---
+++ _config.ts
@@ -0,0 +1,22 @@
+import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
+import metas from "lume/plugins/metas.ts";
+import base_path from "lume/plugins/base_path.ts";
+const site = lume(
+    {
+    src: "./src",
+    server: {
+        open: false,
+    },
+    components: {
+        cssFile: "/assets/css/components.css",
+    },
+});
+
+site.use(metas());
+site.use(postcss());
+site.use(base_path());
+
+  
+
+export default site;
