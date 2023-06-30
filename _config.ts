6dc677a912af15f59fd4b1d6837fea26c26782a1
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-06-30 14:44:29 +0100

    Adding github workflows config

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import metas from "lume/plugins/metas.ts";
 import base_path from "lume/plugins/base_path.ts";
+
 const site = lume(
     {
     src: "./src",
