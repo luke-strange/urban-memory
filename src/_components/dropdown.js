f62afd157c8abdf30ce82869ca0cb3e8af306371
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-07-19 11:42:26 +0100

    Updates closes #1

---
+++ src/_components/dropdown.js
@@ -0,0 +1,8 @@
+export default function myFunction() {
+    var x = document.getElementById("DROPDOWN");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
